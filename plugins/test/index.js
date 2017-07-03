0f24a417422b986757ff60ac49649a0702076d09
yaypm <aliemslie@gmail.com>
2017-07-03 20:50:37 +0100

    Update index.js

--- plugins/test/index.js
+++ plugins/test/index.js
@@ -1,1 +1,65 @@
-test
+'use strict';
+
+/**
+ * The DavisWeather class is the core of the plugin and an
+ * instance of DavisWeather is what will be loaded into Davis
+ */
+class Test {
+
+  /**
+   * The main body of work is done in the constructor.
+   */
+  constructor(davis, options) {
+    this.davis = davis;
+    this.options = options;
+
+    // This is where we declare our intents.
+    this.intents = {
+      // Our intent name
+      test: {
+        // A basic description of the intent
+        usage: 'End the show',
+
+        // Phrases that will trigger our intent. Note that they will not
+        // need to be matched exactly in order for the intent to run.
+        phrases: [
+          'Thanks for your help Davis',
+        ],
+
+        // Lifecycle Events are friendly names for the steps that an intent
+        // needs to take in order to run successfully. For instance, our intent
+        // will need to gather data from the weather underground API, then will
+        // need to respond to the user, so I have broken it up into those events.
+        lifecycleEvents: [
+          'gatherData',
+          'respond',
+        ],
+      },
+    };
+
+    // Hooks give intents functionality.
+    // They are called serially when an intent is run.
+    // They are named using the 'intentName:lifecycleEvent'
+    // Each hook is called with 2 arguments: the exchange object,
+    // and a context object. The exchange object is the primary
+    // interface between Davis, a user, and a plugin. The context
+    // object holds any state carried over from previous exchanges.
+    this.hooks = {
+      'test:gatherData': (exchange, context) => null,
+      'test:respond': (exchange, context) => {
+        const resp = 'My pleasure! As you can see I am quite brilliant, I hope you enjoyed the session and I look forward to hearing from Maurice next.';
+
+        var linkUrl = "https://coffeeconnectionhilliard.files.wordpress.com/2016/05/thank-you-1400x800-c-default.gif";
+	      this.davis.server.pushLinkToUser(exchange.user, linkUrl, true);
+        
+        exchange
+          .response(resp) // respond to the user  
+          .smartEnd() // end the conversation if appropriate
+          .skipFollowUp();
+      },
+    };
+  }
+}
+
+// export the plugin so it can be used
+module.exports = Test;
