cd05984fbe4207ec7caebcddf5c2d04b42d56a29
yaypm <aliemslie@gmail.com>
2017-07-14 21:14:11 +0100

    Update index.js

--- plugins/test/index.js
+++ plugins/test/index.js
@@ -60,7 +60,7 @@ class Test {
 	      	var appmon_password = process.env.APPMON_PASSWORD;
 	      
 		var options = {
-  uri: 'dynatrace.demo.dynatrace.com',
+  uri: 'https://dynatrace.demo.dynatrace.com',
   port: 8021,
   path: '/rest/management/reports/create/Davis%20Test?type=XML&format=XML+Export',
   headers: {'Authorization': 'Basic ' + new Buffer(appmon_username + ':' + appmon_password).toString('base64')},
