d0d3cd4d8321ed8d21916f9406c94d4f3e3d88a2
yaypm <aliemslie@gmail.com>
2017-07-14 20:01:49 +0100

    Update index.js

--- plugins/test/index.js
+++ plugins/test/index.js
@@ -52,30 +52,39 @@ class Test {
 	      	var appmon_username = process.env.APPMON_USERNAME;
 	      	var appmon_password = process.env.APPMON_PASSWORD;
 	      
-	return https.get({
-        host: 'dynatrace.demo.dynatrace.com',
-        path: '/rest/management/reports/create/Davis%20Test?type=XML&format=XML+Export',
-	port: 8021,
-	rejectUnauthorized: false,
-	headers: {'Authorization': 'Basic ' + new Buffer(appmon_username + ':' + appmon_password).toString('base64')}	
-    }, function(response) {
-        // Continuously update stream with data
-        var body = '';
-        response.on('data', function(d) {
-            body += d;
-        });
-        response.on('end', function() {
+	function getAppMonStuff(callback) {
 
-            // Data reception is done, do whatever with it!
-            //var parsed = JSON.parse(body);
-        });
-    });
+    			return https.get({
+        		host: 'dynatrace.demo.dynatrace.com',
+			port: 8021,
+        		path: '/rest/management/reports/create/Davis%20Test?type=XML&format=XML+Export',
+			headers: {'Authorization': 'Basic ' + new Buffer(appmon_username + ':' + appmon_password).toString('base64')},
+			rejectUnauthorized: false	
+    		}, function(response) {
+        	// Continuously update stream with data
+        	var body = '';
+        	response.on('data', function(d) {
+        	    body += d;
+        	});
+        	response.on('end', function() {
+
+        	// Data reception is done, do whatever with it!
+            	//var parsed = JSON.parse(body);
+            	callback({
+                	//email: parsed.email,
+                	//password: parsed.pass
+			figures = body;
+            	});
+        	});
+    	});
+
+},
 	      
 
       },
       'test:respond': (exchange, context) => {
 	     
-        const resp = body;
+        const resp = figures;
         
         exchange
           .response(resp) // respond to the user
