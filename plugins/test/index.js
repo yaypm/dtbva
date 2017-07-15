7f2e118d5b433854780b66c5a9471ff877cccf89
yaypm <aliemslie@gmail.com>
2017-07-15 13:36:46 +0100

    Update index.js

--- plugins/test/index.js
+++ plugins/test/index.js
@@ -91,7 +91,7 @@ class Test {
 	data = test;
 	etree = et.parse(data);
 
-	console.log(etree.findall('./data/textandmeasure/textmeasurecontent')); 
+	console.log(etree.findall('./data/textandmeasure/textmeasurecontent')[2].get('text')); 
 	      
 	exchange
           	.response(test) // respond to the user
