e5ddd7ec4604c829c36d314773b3b5b1f15a3bf1
yaypm <aliemslie@gmail.com>
2017-07-15 12:06:04 +0100

    Update index.js

--- plugins/test/index.js
+++ plugins/test/index.js
@@ -58,25 +58,6 @@ class Test {
 	var appmon_username = process.env.APPMON_USERNAME;
 	var appmon_password = process.env.APPMON_PASSWORD;
 	      
-	//var options = {
-  	//	uri: 'https://dynatrace.demo.dynatrace.com:8021/rest/management/reports/create/Davis%20Test?type=XML&format=XML+Export',
-  	//	headers: {'Authorization': 'Basic ' + new Buffer(appmon_username + ':' + appmon_password).toString('base64')},
-  	//	rejectUnauthorized: false
-	//};
-
-	//function callback(error, response, body) {
- 	//	appMon = body.toString();
-	 	//console.log(appMon);
-		//process.env.APPMONTEST = appMon;
-		//console.log(process.env.APPMONTEST);
-	//	test = appMon;
-	//}
-
-	//request(options, callback);        
-	
-	//console.log('here it comes');      
-	//console.log(process.env.APPMONTEST);
-	      
 	const opts = {
     		uri: 'https://dynatrace.demo.dynatrace.com:8021/rest/management/reports/create/Davis%20Test?type=XML&format=XML+Export',
   		headers: {'Authorization': 'Basic ' + new Buffer(appmon_username + ':' + appmon_password).toString('base64')},
@@ -86,6 +67,7 @@ class Test {
 	return request(opts)
     		.then(resp => {
       		test = resp.toString();
+		console.log(test);
     	})
 	      
       },
